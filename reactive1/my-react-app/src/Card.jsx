82d35b570b79aed8e35312e3afde155935698c23
nayan1306 <vnayan1306@gmail.com>
2024-07-30 11:31:47 +0530

    Added some coool css

--- reactive1/my-react-app/src/Card.jsx
+++ reactive1/my-react-app/src/Card.jsx
@@ -4,8 +4,8 @@ function Card(){
     return(
         <div className="profilecard">
             <img className='card-img' src={profilePic} alt="Profile Picture" />
-            <h3>Nayan Verma</h3>
-            <p>I make cool ML AI stuff then to I am doing this all.</p>
+            <h3 >Nayan Verma</h3>
+            <p className='p-in-card'>I make cool ML AI stuff then to I am doing this all.</p>
         </div>
     )
 }
