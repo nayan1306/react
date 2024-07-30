414d24180321658e81f0e4940cbae4379741018a
nayan1306 <vnayan1306@gmail.com>
2024-07-30 19:11:10 +0530

    Props

--- reactive1/my-react-app/src/App.jsx
+++ reactive1/my-react-app/src/App.jsx
@@ -1,11 +1,14 @@
-import Button from "./components/Button/Button.jsx"
+// import Button from "./components/Button/Button.jsx"
+import Card from "./components/Card.jsx"
 
 function App() {
 
   return(
     <>
-     <Button/>
-
+     <Card name="Nayan Verma" description="I do cool stuff, every hour to make my food digest. This is the key to Crazyness."/>
+     <Card name="Card Two"/>
+     <Card name="Card Three"/>
+     <Card />
     </>
   )
   
--- reactive1/my-react-app/src/components/Card.jsx
+++ reactive1/my-react-app/src/components/Card.jsx
@@ -1,13 +1,26 @@
-import profilePic from './assets/profile_demo.jpeg'
+import profilePic from '../assets/profile_demo.jpeg'
+import PropTypes from 'prop-types';
 
-function Card(){
+function Card(props){
     return(
         <div className="profilecard">
             <img className='card-img' src={profilePic} alt="Profile Picture" />
-            <h3 >Nayan Verma</h3>
-            <p className='p-in-card'>I make cool ML AI stuff then to I am doing this all.</p>
+            <h3 >{props.name}</h3>
+            <p className='p-in-card'>{props.description}</p>
         </div>
     )
 }
+
+Card.propTypes = {
+    name: PropTypes.string,
+    description: PropTypes.string,
+};
+
+Card.defaultProps = {
+    name: "Card Name",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti, harum!"
+}
+
+
 
 export default Card
