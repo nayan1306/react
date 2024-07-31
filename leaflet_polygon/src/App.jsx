564434d54a12ce08c23d5fc09dfd1c0e8b11c1fe
nayan1306 <vnayan1306@gmail.com>
2024-07-31 09:03:40 +0530

    Basic folder struture added

---
+++ leaflet_polygon/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h2>This is soon going to be a leaflet map with polygon drawing feature</h2>
+    </>
+  )
+}
+
+export default App
