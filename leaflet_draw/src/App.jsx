6d35d46c596352758873ec2fa2b89c5971e8d120
nayan1306 <vnayan1306@gmail.com>
2024-07-31 10:05:17 +0530

    Map integrated as an iFrame

---
+++ leaflet_draw/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h2>Polygon Drawing map</h2>
+      <iframe
+        src="/draw/index.html"
+        width="100%"
+        height="800px"
+        style={{ border: "none" }}
+        title="Draw"
+      ></iframe>
+    </>
+  )
+}
+
+export default App
